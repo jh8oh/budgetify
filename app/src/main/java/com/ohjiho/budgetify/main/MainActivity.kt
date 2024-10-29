eac84b93f12e4d05500e3dadbe7c1ce837e05dc8
jh8oh <ohjiho.ca@gmail.com>
2024-10-29 02:52:44 -0400

    Added transaction fab

--- app/src/main/java/com/ohjiho/budgetify/main/MainActivity.kt
+++ app/src/main/java/com/ohjiho/budgetify/main/MainActivity.kt
@@ -2,18 +2,18 @@ package com.ohjiho.budgetify.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewTreeObserver
 import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.ohjiho.budgetify.R
 import dev.ohjiho.budgetify.databinding.ActivityMainBinding
+import dev.ohjiho.budgetify.databinding.DialogAddTransactionBinding
 import dev.ohjiho.budgetify.setup.SetUpActivity
-import dev.ohjiho.budgetify.setup.databinding.ActivitySetUpBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -21,12 +21,33 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
 
+    // Dialog
+    private val addTransactionDialog: AlertDialog by lazy {
+        val binding = DialogAddTransactionBinding.inflate(LayoutInflater.from(this)).apply {
+            newExpense.setOnClickListener {
+                addTransactionDialog.dismiss()
+            }
+
+            newIncome.setOnClickListener {
+                addTransactionDialog.dismiss()
+            }
+
+            newTransfer.setOnClickListener {
+                addTransactionDialog.dismiss()
+            }
+        }
+
+        AlertDialog.Builder(this).apply {
+            setView(binding.root)
+        }.create()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        with(binding){
+        with(binding) {
             root.viewTreeObserver.addOnPreDrawListener(object : ViewTreeObserver.OnPreDrawListener {
                 override fun onPreDraw(): Boolean {
                     checkIfSetUp()
@@ -39,6 +60,9 @@ class MainActivity : AppCompatActivity() {
                 }
             })
 
+            fab.setOnClickListener {
+                addTransactionDialog.show()
+            }
             bottomNav.setupWithNavController((supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment).navController)
         }
     }
