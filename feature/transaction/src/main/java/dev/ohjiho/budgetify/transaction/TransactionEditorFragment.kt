e65b800517892026724ba341f15075922eeb202d
jh8oh <ohjiho.ca@gmail.com>
2024-11-13 01:26:28 -0500

    Created keypad component

--- feature/transaction/src/main/java/dev/ohjiho/budgetify/transaction/TransactionEditorFragment.kt
+++ feature/transaction/src/main/java/dev/ohjiho/budgetify/transaction/TransactionEditorFragment.kt
@@ -7,10 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 import dev.ohjiho.budgetify.domain.model.CategoryType
+import dev.ohjiho.budgetify.theme.component.keypad.Keypad
 import dev.ohjiho.budgetify.transaction.databinding.FragmentTransactionEditorBinding
 
 @AndroidEntryPoint
-class TransactionEditorFragment: Fragment() {
+class TransactionEditorFragment : Fragment(), Keypad.Listener {
 
     private lateinit var binding: FragmentTransactionEditorBinding
 
@@ -26,10 +27,26 @@ class TransactionEditorFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentTransactionEditorBinding.inflate(inflater)
 
+        with(binding) {
+            binding.keypad.setListener(this@TransactionEditorFragment)
+        }
+
         return binding.root
     }
 
-    companion object{
+    override fun onKeyPressed(key: Int) {
+        binding.text.append(key.toString())
+    }
+
+    override fun onDotPressed() {
+        binding.text.append(".")
+    }
+
+    override fun onBackspacePressed() {
+        binding.text.text = binding.text.text.let { if (it.isNotEmpty()) it.substring(0, it.length - 1) else it }
+    }
+
+    companion object {
         private const val CATEGORY_TYPE = "CATEGORY_TYPE"
 
         fun newInstance(categoryType: CategoryType) = TransactionEditorFragment().apply {
