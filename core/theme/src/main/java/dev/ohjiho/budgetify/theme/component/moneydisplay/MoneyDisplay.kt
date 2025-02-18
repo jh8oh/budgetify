e89d010cde4b192d23100c4904c91f674f64012b
jh8oh <ohjiho.ca@gmail.com>
2025-02-17 23:59:45 -0500

    Fixed MoneyDisplay amount setter and getter

--- core/theme/src/main/java/dev/ohjiho/budgetify/theme/component/moneydisplay/MoneyDisplay.kt
+++ core/theme/src/main/java/dev/ohjiho/budgetify/theme/component/moneydisplay/MoneyDisplay.kt
@@ -23,18 +23,15 @@ class MoneyDisplay @JvmOverloads constructor(context: Context, attrs: AttributeS
     var currency: Currency = Currency.getInstance(getLocale(context))
         set(value) {
             field = value
-            decimalDigits = currency.getDecimalAmount()
-            binding.currency.text = currency.currencyCode
-            binding.amount.text = amount.toCurrencyFormat(currency, context)
-        }
-    private var decimalDigits = 2
-        private set(value) {
-            field = value
-            if (value == 0) {
+
+            if (value.getDecimalAmount() == 0) {
                 decimalAmount = ""
-            } else if (decimalAmount.length > value) {
-                decimalAmount.dropLast(decimalAmount.length - value)
+            } else if (decimalAmount.length > value.getDecimalAmount()) {
+                decimalAmount.dropLast(decimalAmount.length - value.getDecimalAmount())
             }
+
+            binding.currency.text = value.currencyCode
+            binding.amount.text = amount.toCurrencyFormat(value, context)
         }
     private val decimalSymbol = DecimalFormatSymbols.getInstance(getLocale(context)).decimalSeparator
 
@@ -46,7 +43,7 @@ class MoneyDisplay @JvmOverloads constructor(context: Context, attrs: AttributeS
     private var amount: String = ""
         private set(value) {
             field = value
-            binding.amount.text = value.toCurrencyFormat(currency, context).split(decimalSymbol)[0]
+            binding.amount.text = value.toCurrencyFormat(currency, context)
         }
     private var decimalAmount: String = ""
         private set(value) {
@@ -60,24 +57,27 @@ class MoneyDisplay @JvmOverloads constructor(context: Context, attrs: AttributeS
         }
 
     init {
-        amount = ""
+        currency = Currency.getInstance(getLocale(context))
+        setAmount(BigDecimal.ZERO)
     }
 
     fun setAmount(amount: BigDecimal) {
         val fullAmount = amount.toCurrencyFormat(currency, context)
         val integerAndDecimal = fullAmount.split(decimalSymbol)
+        val hasDecimal = amount.stripTrailingZeros().scale() > 0
 
         this.amount = integerAndDecimal[0]
-        this.decimalAmount = integerAndDecimal[1]
+        this.decimalAmount = if (hasDecimal) integerAndDecimal[1] else ""
+        inDecimalMode = hasDecimal
     }
 
-    fun getAmount() = BigDecimal("$amount.$decimalAmount")
+    fun getAmount() = BigDecimal("${amount.ifEmpty { "0" }}.${decimalAmount.ifEmpty { "0" }}")
 
     fun addDigit(digit: Int) {
         if (!inDecimalMode) {
             if (amount.isEmpty() && digit == 0) return
             amount += digit
-        } else if (decimalAmount.length < decimalDigits) {
+        } else if (decimalAmount.length < currency.getDecimalAmount()) {
             decimalAmount += digit
         }
     }
