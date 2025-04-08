dda37375c455b1250f4d58173973b9e5fedecc1d
jh8oh <ohjiho.ca@gmail.com>
2025-04-08 01:24:59 -0400

    Minor changes

--- feature/category/src/main/java/dev/ohjiho/budgetify/category/recyclerview/CategoryRecyclerView.kt
+++ feature/category/src/main/java/dev/ohjiho/budgetify/category/recyclerview/CategoryRecyclerView.kt
@@ -2,7 +2,6 @@ package dev.ohjiho.budgetify.category.recyclerview
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dev.ohjiho.budgetify.domain.model.Category
@@ -27,7 +26,6 @@ class CategoryRecyclerView @JvmOverloads constructor(
 
     fun setCategories(isExpense: Boolean, categoryList: List<Category>) {
         (adapter as CategoryAdapter).setCategoryList(isExpense, categoryList)
-        Log.i("asdf", categoryList.joinToString { it.name })
     }
 
     interface Listener {
